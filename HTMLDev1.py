0e8f29b630a701b3837fa559f946f8906ee04799
Brenoww <brenovillasboas2009@gmail.com>
2024-01-11 15:35:44 -0300

    Exercicio HTMLDev1 feito

---
+++ HTMLDev1.py
@@ -0,0 +1,10 @@
+# Declare dois arrays, cada um com um mínimo de cinco elementos, e imprima eles no terminal usando o comando print(). 
+# O primeiro array deve conter os produtos de uma loja da sua escolha (loja de comida, materiais de construção, música, etc). 
+# O segundo array deve conter os anos de nascimento de familiares e amigos seus. Lembre-se de usar nomes descritivos para nomear cada variável,
+# e de usar o tipo de dado apropriado para cada lista (strings, booleanos, números inteiros, floats).
+
+produtos = ["Carne", "Frango", "Peixe", "Melancia", "Uva"];
+dataNasc = [2001,2006,1969,1976,1999];
+
+print(produtos);
+print(dataNasc);
